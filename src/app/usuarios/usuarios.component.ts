6327f4385c0080a4127284c68159e37ce9836c98
Isaias Och <ioch@minfin.gob.gt>
2024-09-24 15:54:57 -0600

    primer commit

---
+++ src/app/usuarios/usuarios.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { UsuariosService } from '../usuarios.service';
+
+@Component({
+  selector: 'app-usuarios',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuarios.component.html',
+  styleUrl: './usuarios.component.css'
+})
+export class UsuariosComponent {
+
+}
